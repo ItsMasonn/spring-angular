d7fbb4eba02f9a1367e13b514983f09076c86db8
Mason Kuenneke <makuenneke@gmail.com>
2023-10-11 19:30:03 -0500

    Added navigation to the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
+import { Router } from '@angular/router';
+
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +12,13 @@ import {HttpClient} from "@angular/common/http";
 export class AppComponent {
   title = 'Demo';
   greeting = {};
-  constructor(private http: HttpClient) {
-    http.get('resource').subscribe(data => this.greeting = data);
+  constructor(private app: AppService, private http: HttpClient, private router: Router) {
+    this.app.authenticate(undefined, undefined);
+  }
+  logout() {
+    this.http.post('logout', {}).finally(() => {
+      this.app.authenticated = false;
+      this.router.navigateByUrl('/login');
+    }).subscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
+import { AppService } from './app.service';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: 'HomeComponent' },
+  { path: 'login', component: 'LoginComponent' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
+    BrowserModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
